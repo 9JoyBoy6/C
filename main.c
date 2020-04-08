481b88767075675585aa849b0cc46471f1acc7d9
9JoyBoy6 <63367431+9JoyBoy6@users.noreply.github.com>
2020-04-08 20:37:54 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <windows.h> // <unistd.h>
+
+typedef struct Slash Slash;
+
+struct Slash {
+
+int type,
+char attaque[50],
+
+};
+
+
+int main(int argc, char *argv[])
+{
+
+    Sleep(2000);
+    printf("Bonjour !\n");
+    Sleep(2000);
+    printf("\nBye !");
+    Sleep(2000);
+
+    return 0;
+}
